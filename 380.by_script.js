a84872406fd12bab216903ac636d57d79acd3b72
trueBellum <romanskidan17@gmail.com>
2023-04-01 00:52:53 +0200

    5

---
+++ 380.by_script.js
@@ -0,0 +1,7 @@
+let backButton = document.getElementById("back");
+let forwardButton = document.getElementById("forward");
+let partners = document.querySelector(".partners")
+
+backButton.addEventListener("click", () =>{
+    
+});
